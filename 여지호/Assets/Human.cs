fcd76df1a89fd1362612be99cbe51704dce0e0ea
GodUser <jijone1005@naver.com>
2020-05-24 23:53:06 +0900

    여지호 이동/점프 1차

---
+++ 여지호/Assets/Human.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+
+    float speed = 5.0f;
+    private  Rigidbody target;
+    
+
+    void Start()
+    {
+        target = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(inputX, 0, 0);
+        target.position = target.position + movement * Time.deltaTime * speed;
+
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            target.AddForce(0, 300, 0);
+        }
+    }
+}
