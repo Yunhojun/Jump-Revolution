d7212ed6a5b8df73c70bbfa1f29dd84c1703e664
GodUser <jijone1005@naver.com>
2020-05-26 18:33:22 +0900

    2D 이동구현

---
+++ 여지호 2D/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody2D Player;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 플레이어 이동 구현
+        float inputX = Input.GetAxis("Horizontal");
+        Vector2 originalState = Player.position;
+        Vector2 move = new Vector2(inputX * speed, originalState.y);
+        Player.position += move * Time.deltaTime;
+
+    }
+}
