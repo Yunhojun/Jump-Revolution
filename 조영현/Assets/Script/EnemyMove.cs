21c141922db2d99756c3fd5f8456e4c16d0a58f7
Younghyun197 <cyh197@naver.com>
2020-06-01 19:36:20 +0900

    장애물

---
+++ 조영현/Assets/Script/EnemyMove.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public int nextMove;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think", 5); // ("함수이름", "초") 주어진 시간이 지난 뒤 지정된 함수를 실행하는 함수
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+
+        // Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.3f, rigid.position.y);      
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider == null)
+            {
+              Turn();
+            }
+        
+
+        }
+
+    //재귀 함수
+    void Think()
+    {
+        //set Next Active
+       nextMove = Random.Range(-1, 2); // -1은 최소에포함, 1은 최대에 포함이 안되므로 2를 써주어야함
+
+        
+
+        //Sprite Animation
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        // Flip sprite
+        if(nextMove !=0)
+            spriteRenderer.flipX = nextMove == 1;
+
+        //Recusive (보통 코드 맨아래 작성)
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", 5);
+
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 5);
+    }
+}
--- 조영현/Assets/Script/PlayerMove.cs
+++ 조영현/Assets/Script/PlayerMove.cs
@@ -21,7 +21,7 @@ public class PlayerMove : MonoBehaviour
    void Update()
     {
         //Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             anim.SetBool("isJumping",true);
@@ -31,7 +31,7 @@ public class PlayerMove : MonoBehaviour
         //Strop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x *0.5f, rigid.velocity.y);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
 
         //Direction Sprite
@@ -60,19 +60,55 @@ public class PlayerMove : MonoBehaviour
         if (rigid.velocity.x > maxSpeed) // Right MAx_Speed
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
 
-        if (rigid.velocity.x < maxSpeed*(-1)) // Left MAx_Speed
-            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+        if (rigid.velocity.x < maxSpeed * (-1)) // Left MAx_Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
 
         //Landing Platform
-        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
-
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1);
-
-        if(rayHit.collider != null)
+        if (rigid.velocity.y < 0)
         {
-            Debug.Log(rayHit.collider.name);
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    anim.SetBool("isJumping", false);
+                }
+
+            }
+
+
         }
+    }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            onDamaged(collision.transform.position);
+        }
+    }
 
+    void onDamaged(Vector2 targetPos)
+    {
+        gameObject.layer = 11;
+
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        int dirc = transform.position.x-targetPos.x > 0 ? 1 :-1;
+        rigid.AddForce(new Vector2(dirc, 1)*7, ForceMode2D.Impulse);
+
+        anim.SetTrigger("deDamgaed");
+
+        Invoke("OffDamaged", 3);
+       
+    }
+
+    void OffDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
     }
 }
