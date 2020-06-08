76cca9ccce0f7a67e539b31f68900ca83d7cb8bb
GodUser <jijone1005@naver.com>
2020-06-08 19:09:07 +0900

    날아다니는 몬스터 구현 및 총 구현

--- 윤호준/Assets/Script/FlyingEnemyMove.cs
+++ 윤호준/Assets/Script/FlyingEnemyMove.cs
@@ -1,26 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class FlyingEnemyMove : MonoBehaviour
 {
-    public float movementScale;
+    public int nextMoveX = 1;
+    public int nextMoveY = 1;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
+    public Vector2 spawnPoint;
 
 
-    void Awake()
+
+
+    void Start()
     {
-        // 몬스터의 처음 스폰위치 설정
-        Vector2 spawnPoint = transform.position;
+
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spawnPoint = rigid.position;
+
     }
 
 
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        
+        //move
+        rigid.velocity = new Vector2(nextMoveX * 3, nextMoveY * 2);
+
+        //flip
+        if (rigid.velocity.x != 0)
+        {
+            spriteRenderer.flipX = nextMoveX == 1;
+        }
     }
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "ScaleX")
+        {
+            nextMoveX *= -1;
+            spriteRenderer.flipX = nextMoveX == 1;
+        }
+
+        if (collision.tag == "ScaleY")
+        {
+            nextMoveY *= -1;
+        }
+
+
+    }
+
 }
