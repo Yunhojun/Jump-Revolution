798c4da2cfd8d1aeb08cdd0d73ed83cb39e09b83
YunHojun <yhj020996@gmail.com>
2020-07-20 19:07:04 +0900

    Revert "Revert "bug fix""
    This reverts commit a34435e334765c774c1439cc311dec6b076e16ab.

---
+++ 윤호준/Assets/Script/Enemy/ExplosiveEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveEnemy : EnemyMove
+{
+    [SerializeField]
+    private float force = 1500;
+
+    protected override void move()
+    {
+        base.move();
+    }
+
+    public override void tread(PlayerMove p)
+    {
+        Rigidbody2D playerRigid = p.GetRig();
+        Vector2 dir = playerRigid.position - rigid.position;
+        playerRigid.AddForce(dir * force);
+        p.stun();
+        //폭발 이펙트
+        Destroy();
+    }
+}
